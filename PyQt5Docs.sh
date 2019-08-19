60a360cc85b57f6a0f57139b1743048560bb2645
Youssif Shaaban Alsager <ysh-alsager@hotmail.com>
2019-08-19 17:13:36 +0200

    Initial commit

---
+++ PyQt5Docs.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+#
+# PyQt5 Dash Docset generator
+# 
+
+# Install requirments
+apt update && apt install -y mercurial python3 python3-pip python3-venv 2> /dev/null
+go get -u github.com/technosophos/dashing
+
+# Create Python venv
+python3 -m venv env
+source env/bin/activate
+pip3 install Sphinx
+
+# Clone docs
+hg clone https://www.riverbankcomputing.com/hg/PyQt5Docs --branch=default
+
+# Start
+current_dir=`echo $(pwd)`
+cd $current_dir/PyQt5Docs/docs/
+sphinx-build -b html . out
+cd out/
+$GOPATH/bin/dashing create
+cp $current_dir/icon.png $current_dir/dashing.json $current_dir/PyQt5Docs/docs/out
+$GOPATH/bin/dashing build pyqt5 2> /dev/null
+tar -cvzf pyqt5.tgz pyqt5.docset
+mv pyqt5.tgz $current_dir
+FILE=$current_dir/pyqt5.tgz
+
+# Upload
+curl -H "Authorization: token $GITHUB_TOKEN" -H "Content-Type: $(file -b --mime-type $FILE)" --data-binary @$FILE "https://uploads.github.com/repos/yshalsager/pyqt5-docset/releases/V5.13/assets?name=$(basename $FILE)"
