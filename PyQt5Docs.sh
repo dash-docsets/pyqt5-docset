59527c2a327784a472de4c4bf533c4a9a7bf8ed7
yshalsager <ysh-alsager@hotmail.com>
2019-08-19 19:26:21 +0200

    release: switch to github-release

--- PyQt5Docs.sh
+++ PyQt5Docs.sh
@@ -6,6 +6,7 @@
 # Install requirments
 apt update && apt install -y mercurial python3 python3-pip python3-venv file 2> /dev/null
 go get -u github.com/technosophos/dashing
+go get -u github.com/aktau/github-release
 
 # Create Python venv
 python3 -m venv env
@@ -27,5 +28,5 @@ tar -cvzf pyqt5.tgz pyqt5.docset
 mv pyqt5.tgz $current_dir
 FILE=$current_dir/pyqt5.tgz
 
-# Upload
-curl -H "Authorization: token $GITHUB_TOKEN" -H "Content-Type: $(file -b --mime-type $FILE)" --data-binary @$FILE "https://uploads.github.com/repos/yshalsager/pyqt5-docset/releases/V5.13/assets?name=$(basename $FILE)"
+# Upload to GitHub
+$GOPATH/bin/github-release upload -u yshalsager -r pyqt5-docset -t "V5.13" -n $FILE -f $FILE
